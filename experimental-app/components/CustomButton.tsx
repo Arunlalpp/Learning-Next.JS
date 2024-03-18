f0e84ba8a9b816e31012ccc51bba5d2644eacd17
Arunlalpp <arunlalpp@gmail.com>
2024-03-18 18:00:49 +0530

    feat: add footer and navbar component

--- experimental-app/components/CustomButton.tsx
+++ experimental-app/components/CustomButton.tsx
@@ -1,14 +1,16 @@
+"use client";
 import { CustomButtonProps } from "@/types";
 
 const CustomButton = ({
   title,
   containerStyles,
   handleClick,
+  buttonType,
 }: CustomButtonProps) => {
   return (
     <button
       disabled={false}
-      type={"button"}
+      type={buttonType || "button"}
       className={`custom-btn ${containerStyles}`}
       onClick={() => handleClick}
     >
